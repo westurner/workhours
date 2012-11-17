318b664c116c8e12372a6cf0887cc178a00055b7
Wes Turner <50891+westurner@users.noreply.github.com>
2012-11-17 14:07:05 -0600

    Wrapped firefox/bookmarks.py in a main()

--- workhours/firefox/bookmarks.py
+++ workhours/firefox/bookmarks.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
-from BeautifulSoup import BeautifulSoup as BS
 
-b = BS(file("bookmarks.html").read())
+def main():
+    from BeautifulSoup import BeautifulSoup as BS
+
+    b = BS(file("bookmarks.html").read())
 
 
-print '\n'.join(map(lambda x: "== %s ==\n%s\n" % (urllib.unquote_plus(x.string), urllib.unquote_plus(x.get('href'))), filter(lambda x: x.string.startswith("&quot"), b.findAll("a"))))
+    print '\n'.join(map(lambda x: "== %s ==\n%s\n" % (urllib.unquote_plus(x.string), urllib.unquote_plus(x.get('href'))), filter(lambda x: x.string.startswith("&quot"), b.findAll("a"))))
 
+if __name__=="__main__":
+    main()
