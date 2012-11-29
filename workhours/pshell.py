6b30aa60f77d37a7a826e49774401be5b8f32c62
Wes Turner <wes@wrd.nu>
2012-11-29 04:08:24 -0600

    Updated workhours/pshell.py: Commenting out pshell setup and imports

--- workhours/pshell.py
+++ workhours/pshell.py
@@ -1,8 +1,8 @@
-from workhours.models.sql import engine
-from workhours.models.sql import initialize_sql
-from workhours.models import setup_mappers
+#from workhours.models.sql import engine
+#from workhours.models.sql import initialize_sql
 
 def setup(*args, **kwargs):
-    setup_mappers(engine)
-    initialize_sql(engine)
+#    setup_mappers(engine)
+    #initialize_sql(engine)
+    pass
 
