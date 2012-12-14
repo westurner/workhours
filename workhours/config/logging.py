d2dbc5db1a54ae7c8a15d55ac6b5fb30c68eb00f
Wes Turner <wes@wrd.nu>
2012-12-14 00:41:59 -0600

    Added workhours.config.logging: configure_logging -> logging.fileConfig

---
+++ workhours/config/logging.py
@@ -0,0 +1,10 @@
+import logging
+TRACE = 3
+
+def configure_logging(configfile):
+    logging.fileConfig(configfile)
+    logging.addLevelName(TRACE, 'TRACE')
+
+
+def add_trace_logging(log):
+    log.addLevelName(TRACE, 'TRACE')
