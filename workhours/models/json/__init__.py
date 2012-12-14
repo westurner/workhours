696f7fd90f977be9389f82077530c536eab36bac
Wes Turner <wes@wrd.nu>
2012-12-14 00:41:59 -0600

    Updated workhours.models.json: check for hasattr('_asdict')

--- workhours/models/json/__init__.py
+++ workhours/models/json/__init__.py
@@ -1,23 +1,23 @@
 
-import json
 import datetime
-datetime_types = (datetime.time, datetime.date, datetime.datetime)
 import decimal
-from sqlalchemy.util import NamedTuple as sqla_namedtuple
-from collections import namedtuple
-named_tuple_types = (namedtuple, sqla_namedtuple, )
+import json
+datetime_types = (datetime.time, datetime.date, datetime.datetime)
+
+#from collections import namedtuple
+#from sqlalchemy.util import NamedTuple as sqla_namedtuple
+#named_tuple_types = (namedtuple, sqla_namedtuple, )
 
 #import networkx as nx
 #graph_types = (nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph)
 #from networkx.readwrite.json_graph import node_link_data
 
-
-
-
 class DefaultJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         """Convert ``obj`` to something JSON encoder can handle."""
-        if hasattr(obj, '_fields'): #isinstance(obj, named_tuple_types):
+        if hasattr(obj, '_asdict'):
+            obj = obj._asdict()
+        elif hasattr(obj, '_fields'): #isinstance(obj, named_tuple_types):
             obj = dict((k, getattr(obj, k)) for k in obj.keys())
         elif isinstance(obj, decimal.Decimal):
             obj = str(obj)
