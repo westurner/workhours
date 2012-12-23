38d96cad09e3a734449403d7fd8313003e6242da
Wes Turner <wes@wrd.nu>
2012-12-23 05:17:34 -0600

    TODO: id->_id, .events.tests.test_models: fixtures, datatable attributes * * * FOLD: Updated workhours/events/tests/test_models.py

--- workhours/events/tests/test_models.py
+++ workhours/events/tests/test_models.py
@@ -13,30 +13,24 @@ from pyramid_restler.model import SQLAlchemyORMContext
 
 import workhours.models
 from workhours.models.sql import _initialize_sql_test
-def patch_tests(**kwargs):
-    testing.DummyRequest.db_session = workhours.models.DBSession
-    for (k,v) in kwargs.iteritems():
-        setattr(testing.DummyRequest, k, v)
 
+from workhours.testing import PyramidFixtureTestCase
+from workhours.models.fixtures import data
 
 import transaction
 
 #IContextFactory
-class EventsContextTests(unittest.TestCase):
+class EventsContextTests(PyramidFixtureTestCase):
+    fixtures = (data.EventData,)
     def setUp(self):
+        super(EventsContextTests, self).setUp()
+        #req = testing.DummyRequest()
+        #self.config = testing.setUp(req)
+        #self.meta = _initialize_sql_test()
+        #req.db_session = self.meta.Session
+        self.request = self._new_request()
+        self.context = EventsContextFactory(self.request)
 
-        self.config = testing.setUp()
-        self.meta = _initialize_sql_test()
-        self.session = self.meta.Session
-        patch_tests(db_session=self.session)
-        req = testing.DummyRequest()
-        req.db_session.commit = transaction.commit
-        self.context = EventsContextFactory(req)
-
-    def tearDown(self):
-        import transaction
-        transaction.abort()
-        testing.tearDown()
 
     def test_context(self):
         self.assertTrue(self.context)
@@ -48,56 +42,64 @@ class EventsContextTests(unittest.TestCase):
         self.failIfEqual(results, None)
 
     def test_get_member(self):
-        self.context.get_member("17")
-        self.context.get_member("32")
+        obj = self.context.get_member(data.EventData.one._id)
+        self.assertIsNotNone(obj)
+
         #fails:
-        # self.context.get_member(17)
+        for id in [None,'NONEXISTANT']:
+            self.failUnlessRaises(Exception,
+                lambda id: self.context.get_member(id))
         # self.context.get_member(-1)
 
     # NOP
-    def test_create_member(self):
-        func = lambda : self.context.create_member({})
-        self.failUnlessRaises(NotImplementedError, func)
-        # should fail
-    def test_update_member(self):
-        func = lambda : self.context.update_member(-1, {})
-        self.failUnlessRaises(NotImplementedError, func)
-        # should fail
-    def test_delete_member(self):
-        id = 0
-        obj = self.context.get_member(id)
-        self.context.delete_member(id)
-        func = lambda: self.context.get_member(id)
-        self.failUnlessRaises(NotImplementedError, func)
-        # should fail
+    #def test_create_member(self):
+    #    self.context.create_member()
 
-    def test_get_member_id_as_string(self):
-        self.assertEqual(
-            "10",
-            self.context.get_member_id_as_string({'n': "10"}))
+    #def test_update_member(self):
+    #    self.context.update_member(-1, {})
+
+    def test_delete_member(self):
+        id = data.EventData.one._id
+        obj = self.context.get_member(id)
+        self.assertIsNotNone(obj)
+        self.context.delete_member(id)
+        self.session.flush()
+
+        obj = self.context.get_member(id)
+        self.assertIsNone(obj)
+
+    # TODO: ?
+    #def test_get_member_id_as_string(self):
+    #    self.assertEqual(
+    #        "10",
+    #        self.context.get_member_id_as_string({'n': "10"}))
 
     def test_to_json(self):
-        import workhours.models.json as json
-        #value = dict(one='two', two='three')
-        #jsonstr = self.context.to_json(value)
-        #self.assertTrue(json.loads(jsonstr))
-        ID=0
-        obj = self.context.get_member(ID)
+        obj = self.context.get_member(data.EventData.one._id)
+        self.assertIsNotNone(obj)
         self.context.to_json(obj)
 
     #def test_get_json_obj(self):
     #    self.context.get_json_obj(value, fields, wrap)
 
     def test_wrap_json_obj(self):
-        obj = ['one','two','three']
-        outp = dict(results=obj, result_count=len(obj))
+        #obj = ['one','two','three']
+        #outp = dict(results=obj, result_count=len(obj))
+        ID = data.UserData.one._id
+        obj = self.context.get_member(ID)
+        outp = dict(
+                results=obj,
+                result_count=len(obj),
+                iTotalDisplayRecords=(1,),  # datatables
         self.assertEqual(self.context.wrap_json_obj(obj), outp)
 
     def test_member_to_dict(self):
-        member = ['one','two','three']
+        ID = data.UserData.one._id
+        obj = self.context.get_member(ID)
+        self.assertIsNotNone(obj)
         self.assertEqual(
-                member,
-                self.context.member_to_dict(member)
+                obj,
+                self.context.member_to_dict(obj)
         )
         # TODO
         # self.context.member_to_dict(member, fields=['one'])
