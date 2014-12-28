d0a09f629992b3f0fe21f7a18d49d527a91f4ec9
Wes Turner <wes@wrd.nu>
2014-12-28 07:20:02 -0600

    MERGE `pcreate -s angularjs_w_user workhours` on top

--- setup.py
+++ setup.py
@@ -1,66 +1,49 @@
-# workhours setup.py file
+import os
+
 from setuptools import setup, find_packages
-import os.path as osp
-version = '0.4.01'
 
-def rel_path(path_):
-    return osp.join(osp.dirname(osp.abspath(__file__)), path_)
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.md')) as f:
+    CHANGES = f.read()
 
-
-from setuptools.command.test import test as TestCommand
-
-#class PyTest(TestCommand):
-#    def finalize_options(self):
-#        TestCommand.finalize_options(self)
-#        self.test_args = []
-#        self.test_suite = True
-#    def run_tests(self):
-#        #import here, cause outside the eggs aren't loaded
-#        import pytest
-#        pytest.main(self.test_args)
+requires = [
+    'pyramid',
+    'waitress',
+    'sqlalchemy',
+    'alembic',
+    'pyramid_jinja2',
+    'passlib',
+    'colander',
+    'pyramid_marrowmailer',
+    'html2text',
+    'pyramid_tm',
+    'sqlalchemy-utils'
+    ]
 
 setup(name='workhours',
-    version=version,
-    description="Aggregates events from various sources",
-    #long_description='\n'.join( (
-    #    file(rel_path('README.rst'),'rb').read(),
-    #    file(rel_path('CHANGES.rst'), 'rb').read(),) ),
-    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='',
-    author='Wes Turner',
-    author_email='wes@wrd.nu',
-    url='',
-    license='New BSD',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    package_data={
-        'workhours': [
-            'README.rst',
-            'CHANGES.rst',
-            'scripts/*.sh',] },
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        # -*- Extra requirements: -*-
-        'SQLAlchemy',
-        'pytz>=2010e',
-        'BeautifulSoup',
-        'pyutmp',
-        'pyes',
-
-        'pyramid',
-        'jinja2',
-        'zope.sqlalchemy',
-        'passlib'
+      version='0.0',
+      description='workhours',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         ],
-    entry_points={
-        # -*- Entry points: -*-
-        'paste.app_factory': [
-            'main = workhours:main'
-        ],
-        'console_scripts': [
-            'workhours = workhours.climain:main',
-            ]
-        },
-    tests_require=['nose', 'fixture'],
-    #cmdclass = {'test': PyTest},
-)
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="test",
+      entry_points="""\
+      [paste.app_factory]
+      main = workhours:main
+      """,
+      )
--- workhours/__init__.py
+++ workhours/__init__.py
@@ -1,261 +1,90 @@
 from pyramid.config import Configurator
-from pyramid.authentication import AuthTktAuthenticationPolicy
-from pyramid.authorization import ACLAuthorizationPolicy
-from pyramid.session import UnencryptedCookieSessionFactoryConfig
-from pyramid.renderers import JSONP
-from pyramid.events import subscriber
-from pyramid.events import NewRequest, ApplicationCreated
+from pyramid import authentication, authorization
 
 from sqlalchemy import engine_from_config
-import workhours.models
-from workhours.models.sql import initialize_sql
-from workhours.models.es import initialize_esdb
-from workhours.models.files import initialize_fs
-#from .models.rdf import initialize_rdflib
-
+from sqlalchemy.orm import sessionmaker
+from .resource import APIRoot 
+from .security import get_principals
+from .models import User
+from ConfigParser import ConfigParser
 import logging
-log = logging.getLogger('workhours')
+
+log = logging.getLogger(__name__)
+
+def db(request):
+    """every request will have a session associated with it. and will
+    automatically rollback if there's any exception in dealing with
+    the request
+    """
+    maker = request.registry.dbmaker
+    session = maker()
+
+    def cleanup(request):
+        if request.exception is not None:
+            session.rollback()
+        else:
+            session.commit()
+        session.close()
+
+    request.add_finished_callback(cleanup)
+
+    return session
+
+def authenticated_user(request):
+    def x():
+        return request.db.query(User).filter_by(id=request.authenticated_userid).first()
+    return x
+
+def config_static(config):
+    config.add_static_view('static', 'static', cache_max_age=3600)
 
 
-
-def _connect_sqldb(request):
-    #conn = request.registry.dbsession() #
-    sql_url = request.registry.settings.get('db_main.url').strip()
-
-    if sql_url:
-        engine = engine_from_config(
-                request.registry.settings,
-                'db_main.')
-
-        def setup(*args):
-            from workhours.models import Base
-            from workhours.models.sql.tables import setup_mappers
-            return initialize_sql(
-                    engine=engine,
-                    setup_mappers=setup_mappers,
-                    Base=Base)
-        request.set_property(setup, 'meta')
-
-        conn = request.meta.Session # TODO
-        # see: transaction.commit
-        #def cleanup(_):
-        #    log.debug('close sqldb')
-        #    #conn.close()
-        #    TODO: list session instances
-        #request.add_finished_callback(cleanup)
-        return conn
-    raise Exception('db_main.url not configured')
-
-
-def _connect_esdb(request):
-    esdb_url = request.registry.settings.get('esdb.url').strip()
-    if esdb_url:
-        conn = initialize_esdb(esdb_url)
-        def cleanup(_):
-            log.debug('close esdb')
-            conn.close()
-        request.add_finished_callback(cleanup)
-        return conn
-    raise Exception('esdb.url not configured')
-
-
-def _connect_fs(request):
-    fs_url = request.registry.settings.get('fs.url').strip()
-    if fs_url:
-        fs = initialize_fs(fs_url)
-        return fs
-    raise Exception('fs.url not configured')
-
-
-def _connect_rdflib(request):
-    try:
-        import rdflib
-    except ImportError, e:
-        log.error('import rdflib')
-
-    rdf_url = request.registry.settings.get('rdflib.virtuoso_connstr')
-    if rdf_url:
-        conn = initialize_rdflib(
-                    virtuoso_connstr=settings['rdflib.virtuoso_connstr'])
-        return rdflib
-
-
-#@subscriber(ApplicationCreated)
-#def example_config(app):
-#   pass
-
-@subscriber(NewRequest)
-def db_session_subscriber(event):
-    request = event.request
-    request.set_property(_connect_sqldb, 'db_session', reify=True)
-    request.set_property(_connect_esdb, 'es_session', reify=True)
-    #event.request.db_session = workhours.models.DBSession
-
-
-
-def string_response_adapter(s):
-    response = Response(s)
-    response.content_type = 'text/html'
-    return response
-
-
-
-import os
-from pyramid.response import FileResponse
-
-def favicon_view(request):
-    here = os.path.dirname(__file__)
-    icon = os.path.join(here, 'static', 'favicon.ico')
-    return FileResponse(icon, request=request)
-
-
-def configure_routes(config):
-    config.add_static_view('static', 'workhours:static')
-    config.add_route('favicon', '/favicon.ico')
-    config.add_view('workhours.favicon_view', name='favicon')
-
-    config.include('deform_jinja2')
-
-    ## Security Routes
-    config.add_route('register', '/register')
-    config.add_route('login', '/login')
-    config.add_route('logout', '/logout')
-    config.add_route('user', '/user')
-
-    ## Site Routes
-    config.add_route('about', '/about')
-    config.add_route('main', '/')
-
-    ## API Routes
-    config.include('pyramid_restler')
-    config.enable_POST_tunneling()
-
-    from workhours.events.views import EventsContextFactory
-    from workhours.events.views import EventsRESTfulView
-    config.add_restful_routes('api/events', EventsContextFactory,
-                                    view=EventsRESTfulView)
-
-    from workhours.places.models import PlacesContextFactory
-    from workhours.places.views import PlacesRESTfulView
-    config.add_restful_routes('api/places', PlacesContextFactory,
-                                    view=PlacesRESTfulView)
-
-    from workhours.reports.places import (
-            DomainsContext,
-            ProjectsContext,
-            WikipediaPagesContext)
-    from workhours.reports.views import (
-            DomainsView,
-            ProjectsView,
-            WikipediaPagesView)
-    config.add_restful_routes('reports/domains', DomainsContext,
-                                    view=DomainsView)
-    config.add_restful_routes('reports/projects', ProjectsContext,
-                                    view=ProjectsView)
-    config.add_restful_routes('reports/wikipedia', WikipediaPagesContext,
-                                    view=WikipediaPagesView)
-
-    #from workhours.forms import pyramid_csrf_demo
-    config.add_route('pyramid_csrf_demo', '/formdemo')
-
-    #config.add_route('sparql_query', '/sparql')
-    #config.add_route('deniz', '/browse')
-    config.include('pyramid_debugtoolbar')
-
-
-
-def configure_renderers(config):
-    config.add_renderer('jsonp', JSONP(param_name='callback'))
-
-
-def configure_jinja2(config):
-    from .site.templatefilters import skipautoescape, jsonify, jsonify_indent
+def config_jinja2(config):
     config.include('pyramid_jinja2')
-    #config.add_jinja2_renderer('.jinja2')
-    #config.add_jinja2_search_path('templates', name='.jinja2')
-
-    env = config.get_jinja2_environment()
-    if env is not None:
-        # env.extensions
-        env.filters['skipautoescape'] = skipautoescape
-        env.filters['jsonify'] = jsonify
-        env.filters['jsonify_indent'] = jsonify_indent
-    else:
-        logging.error('get_jinja2_environment() -> None')
+    config.add_jinja2_renderer('.html')
+    config.add_jinja2_search_path('templates', name='.html')
 
 
-def configure_auth_views(config):
+def config_mailer(config):
+    config.include('pyramid_marrowmailer')
+    config.include('pyramid_tm')
 
-    config.add_view('workhours.site.views.errors.http404',
-            renderer='workhours:templates/http404.jinja2',
-            context='pyramid.exceptions.NotFound')
+def config_db(config, settings):
+    # configure database with variables sqlalchemy.*
+    engine = engine_from_config(settings, prefix="sqlalchemy.")
+    config.registry.dbmaker = sessionmaker(bind=engine)
 
-    config.add_view('workhours.site.views.errors.http403',
-            renderer='workhours:templates/http403.jinja2',
-            context='pyramid.exceptions.Forbidden')
-
-    config.testing_add_renderer('templates/login.jinja2')
-    config.testing_add_renderer('templates/toolbar.jinja2')
-
-
-def configure_app_views(config):
-    #config.testing_add_renderer('templates/cloud.jinja2')
-    #config.testing_add_renderer('templates/latest.jinja2')
-    #config.testing_add_renderer('templates/sparql_query.jinja2')
-    return config
+    # add db session to request
+    config.add_request_method(db, reify=True)
 
 
-def configure_test_app(config):
-    configure_renderers(config)
-    configure_jinja2(config)
-    configure_auth_views(config)
-    configure_app_views(config)
-    configure_routes(config)
-    return config
-
-
-def configure_app(settings, authn_policy, authz_policy, session_factory):
-    config = Configurator(
-        settings=settings,
-        root_factory='workhours.models.RootFactory',
-        authentication_policy=authn_policy,
-        authorization_policy=authz_policy,
-        session_factory=session_factory,
-    )
-    config.add_translation_dirs('locale/')
-
-    config.add_response_adapter(string_response_adapter, basestring)
-
-    configure_test_app(config)
-
-    config.add_subscriber('workhours.security.csrf.csrf_validation',
-                          'pyramid.events.NewRequest')
-
+def config_routes(config):
+    config.add_route('home', '/')
     config.scan()
-    configure_routes(config)
+    config.add_route("api", '/api/*traverse', factory=APIRoot)
 
-    return config
+def config_auth_policy(config, settings):
+    policy = authentication.AuthTktAuthenticationPolicy(settings['auth_secret'], get_principals, cookie_name="workhours_auth", hashalg="sha512")
+    config.set_authentication_policy(policy)
+    config.set_authorization_policy(authorization.ACLAuthorizationPolicy())
 
+def config_secrets(settings):
+    if "secrets" in settings:
+        try:
+            config = ConfigParser()
+            config.read(settings["secrets"])
+            settings.update(config.items("secrets"))
+        except:
+            log.warn("secrets were specificed in the configuration but could not be read\n\n%s" % settings.get("secrets", ""), exc_info=1)
 
 def main(global_config, **settings):
-    """ This function returns a Workhours Pyramid WSGI application.
-    """
-
-    log.debug(settings)
-
-    session_factory = UnencryptedCookieSessionFactoryConfig('secret')
-
-    authn_policy = AuthTktAuthenticationPolicy('s0secret')
-    authz_policy = ACLAuthorizationPolicy()
-
-    settings.setdefault('jinja2.i18n.domain', 'workhours')
-
-    config = configure_app(
-        settings,
-        authn_policy,
-        authz_policy,
-        session_factory)
-
+    config_secrets(settings)
+    config = Configurator(settings=settings)
+    config_static(config)
+    config_jinja2(config)
+    config_db(config, settings)
+    config_routes(config)
+    config_auth_policy(config, settings)
+    config_mailer(config)
+    config.add_request_method(authenticated_user, reify=True)
     return config.make_wsgi_app()
-
-app = main
