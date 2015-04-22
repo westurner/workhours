97a0b5f5151309e8ea20bb9e44ca4df644600bd3
Wes Turner <50891+westurner@users.noreply.github.com>
2015-04-22 09:05:00 -0500

    BUG,ENH,REF,CLN: datatables, as-is

--- workhours/site/views/errors.py
+++ workhours/site/views/errors.py
@@ -1,8 +1,12 @@
 
+#@view_config(context=HTTPNotFound, renderer='404.jinja')
 def http404(request):
+    request.response.status = 404
     return {'title': '404 Not Found',
             'errcode': '404'}
 
+#@view_config(renderer='403.jinja')
 def http403(request):
+    request.response.status = 403
     return {'title': '403 Forbidden',
             'errcode': '403'}
